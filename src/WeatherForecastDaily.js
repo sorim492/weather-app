a002a78cd8b60e2c084ca0eddc30c56881dca424
sorim492 <sorim492@gmail.com>
2023-12-16 19:22:18 -0600

    Update WeatherForecastDaily.js

--- src/WeatherForecastDaily.js
+++ src/WeatherForecastDaily.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default function WheatherForecastDaily(props){
+    function day(){
+        let days=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        let date = new Date(props.data.dt * 1000);
+        let day = days[date.getDay()];
+        
+        return day;
+    }
+
     return(
     <div>
-        <h2>{props.data.dt}</h2>
+        <h2>{day()}</h2>
         <span>{Math.round(props.data.temp.max)}°C {" | "}</span>
         <span>{Math.round(props.data.temp.min)}°C</span>
     </div>
